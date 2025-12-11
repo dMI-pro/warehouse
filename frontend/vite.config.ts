b5134456c4ee05fbe1bfd6ab9432bbc403d5ff20
Prokhvataev Dmitriy Alexandrovich <sorktt@mail.ru>
2025-12-11 11:10:34 +0300

    fix: watching frontend and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,4 +15,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true, // Необходимо для Docker на Windows
+      interval: 1000,   // Проверка изменений каждую секунду
+    },
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+    },
+  },
 })
