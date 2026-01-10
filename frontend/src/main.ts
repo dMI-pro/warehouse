bff4f20e182e0b418e3d7e996e8b79ca13136472
Prokhvataev Dmitriy Alexandrovich <sorktt@mail.ru>
2026-01-10 23:42:20 +0300

    refactor: added npm primeflex, fixed layouts productDetailsView

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,6 +7,7 @@ import Tooltip from 'primevue/tooltip';
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 import App from './App.vue';
 import router from './router';
